37a74ba4a437b54d332f7640e3600483015ad943
Mika <marikadriman@gmail.com>
2018-06-21 13:55:32 +0200

    change joke and adjust layout

--- javajokes/src/main/java/com/example/android/javajokes/Joker.java
+++ javajokes/src/main/java/com/example/android/javajokes/Joker.java
@@ -3,6 +3,6 @@ package com.example.android.javajokes;
 public class Joker {
 
     public static String getJoke() {
-        return "An Irishman walks out of a bar";
+        return "I was going to tell you a time travelling joke, but you didn't like it";
     }
 }
